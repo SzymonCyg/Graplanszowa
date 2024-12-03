026d652c7aa47b57ed38fb134a74f3ca3340e045
szymon <sda@sda/pl>
2024-12-03 09:06:19 +0100

    utworzenie klas i interfejsów

--- Gra planszowa/Program.cs
+++ Gra planszowa/Program.cs
@@ -1,7 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.ComponentModel;
 
+class Player
+{
+    public string Name;
+    public int Position { get; set; }
+    public int Score { get; set; }
 
+    void Ruch(int x , int y)
+    {
+        
+    }
+
+    public void akt(int score)
+    {
+        Score = score;
+    }
+}
+
+class Board
+{
+    public int rozmiarx;
+    public int rozmiary;
+
+    public int nagroda(int x, int y)
+    {
+        
+    }
+}
+
+class Game
+{
+    void przebiegGry()
+    {
+        
+    }
+
+    void poleNagroda()
+    {
+        
+    }
+
+    public int wynik()
+    {
+        
+    }
+}
+
+interface IWojownik
+{
+    
+}
+
+interface IMag
+{
+    
+}
+
+interface IHealer
+{
+    
+}
 internal class Program
 {
     public static void Main(string[] args)
