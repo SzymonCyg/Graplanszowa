719c46067a90f0626a9bdd448533ab03f367d0aa
szymon <mail@example.com>
2024-12-07 22:09:14 +0100

    utworzenie metody wynik oraz rozpoczecie pracy nad klasa board

--- Gra planszowa/Program.cs
+++ Gra planszowa/Program.cs
@@ -2,61 +2,74 @@
 
 using System.ComponentModel;
 
-// class Player
-// {
-//     public string Name { get; set; }
-//     public int Position { get; set; }
-//     public int Score { get; set; }
-//
-//     void Ruch(int x , int y)
-//     {
-//         
-//     }
-//
-//     public void akt(int score)
-//     {
-//         Score = score;
-//     }
-// }
+class Player
+{
+    public string Name { get; set; }
+    public int Position { get; set; }
+    public int Score { get; set; }
 
-// class Board
-// {
-//     public int rozmiarx;
-//     public int rozmiary;
-//
-//     public int nagroda(int x, int y)
-//     {
-//         
-//     }
-// }
+    void Ruch(int x , int y)
+    {
+        
+    }
+
+    public void akt(int score)
+    {
+        Score = score;
+    }
+}
+
+class Board{
+
+    int plansza = 64;
+
+    public int nagroda(int x, int y)
+    {
+        
+    }
+}
 
 class Game
-{
-    // void przebiegGry()
-    // {
-    //     
-    // }
+{ 
+    public int[] lokalizacjaNagroda = new int[20];
+    public int poleGracz= int 0;
+    public void Gra()
+    {
+        Random losowanie = new Random();
+        int poleGraczDodaj=losowanie.Next(1,7);
+        poleGracz =poleGracz+poleGraczDodaj;
+        if(poleGracz>64)
+        {
+            poleGracz=poleGracz% 64;
+        }
+    }
 
     public void poleNagroda()
     {
-        int[] lokalizacjanagroda = new int[20];
+ 
         Random losowanie = new Random();
         for (int i = 0; i < 20; i++)
         {
             int x =losowanie.Next(1, 65);
-            lokalizacjanagroda[i] = x;
+            lokalizacjaNagroda[i] = x;
         }
-        foreach (int liczba in lokalizacjanagroda)
-        {
-            Console.WriteLine(liczba);
-        }
-
     }
 
-    // public int wynik()
-    // {
-    //     
-    // }
+    public int wynik()
+    {
+        int wynik = 0;
+
+        foreach (int nagroda in lokalizacjaNagroda)
+        {
+            if (nagroda == poleGracz) 
+            {
+                wynik += 1; 
+            }
+        }
+
+        return wynik; 
+    }
+
 }
 
 // interface IWojownik
